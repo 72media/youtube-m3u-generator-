71d12042698ccc66de2f5bc18f48d81de5e9eaa3
hs8tv <2ramrome@gmail.com>
2025-06-11 20:36:19 +0700

    Add files via upload

---
+++ create_playlist.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: ชื่อไฟล์ M3U
+set OUTPUT=%~dp0playlist.m3u
+
+:: ลบไฟล์เก่า
+if exist "%OUTPUT%" del "%OUTPUT%"
+
+:: เขียนหัวไฟล์
+echo #EXTM3U >> "%OUTPUT%"
+
+:: ท่าเรือ หาดบางรัก
+for /f "delims=" %%A in ('yt-dlp -g https://www.youtube.com/watch?v=jrYuqZmrV-4') do (
+    echo #EXTINF:-1,ท่าเรือ หาดบางรัก>> "%OUTPUT%"
+    echo %%A>> "%OUTPUT%"
+    goto next1
+)
+:next1
+
+:: Jimmy Woo's
+for /f "delims=" %%A in ('yt-dlp -g https://www.youtube.com/watch?v=j4D0N8Pevwg') do (
+    echo #EXTINF:-1,Jimmy Woo's>> "%OUTPUT%"
+    echo %%A>> "%OUTPUT%"
+    goto next2
+)
+:next2
+
+:: Crystal Bay
+for /f "delims=" %%A in ('yt-dlp -g https://www.youtube.com/watch?v=Fw9hgttWzIg') do (
+    echo #EXTINF:-1,Crystal Bay>> "%OUTPUT%"
+    echo %%A>> "%OUTPUT%"
+    goto done
+)
+:done
+
+echo Playlist updated: %OUTPUT%
